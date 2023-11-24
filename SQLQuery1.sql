cb82045f105d0bde9e7a603a2888720e831e3348
victorchongwa <134845356+victorchongwa@users.noreply.github.com>
2023-11-24 12:02:50 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,24 @@
+--create table employeedemo(
+--employeeid int,
+--firstname varchar(50),
+--lastname varchar(50),
+--age int,
+--gender varchar(50)
+--)
+ --second table--
+ --create table employeesalary(
+ --employeeid int,
+ --jobtitle varchar(50),
+ --salary int,
+ --)
+ 
+ insert into [dbo].[employeedemo] values
+ (1001, 'Jim', 'Halpert', 30, 'Male'),
+(1002, 'Pam', 'Beasley', 30, 'Female'),
+(1003, 'Dwight', 'Schrute', 29, 'Male'),
+(1004, 'Angela', 'Martin', 31, 'Female'),
+(1005, 'Toby', 'Flenderson', 32, 'Male'),
+(1006, 'Michael', 'Scott', 35, 'Male'),
+(1007, 'Meredith', 'Palmer', 32, 'Female'),
+(1008, 'Stanley', 'Hudson', 38, 'Male'),
+(1009, 'Kevin', 'Malone', 31, 'Male')
